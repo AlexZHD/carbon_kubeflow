5d9781e0fb3a332688e650484124bdb9c917e7f1
Alex <azmachynski@gmail.com>
2019-06-21 16:31:16 -0700

    docker optimized

--- food_facts/app.py
+++ food_facts/app.py
@@ -2,13 +2,14 @@ from os import environ
 from joblib import load
 from flask import abort, Flask, jsonify, make_response, request
 import numpy as np
-# service_name = environ['SERVICE_NAME']
-# version = environ['API_VERSION']
+service_name = environ['SERVICE_NAME']
+version = environ['API_VERSION']
 trans = load('transformer.joblib')
 model = load('model.joblib')
+print(f'/{service_name}/v{version}/predict')
 app = Flask(__name__)
-#@app.route(f'/{service_name}/v{version}/predict', methods=['POST'])
-@app.route('/carbon/1/predict', methods=['POST'])
+@app.route(f'/{service_name}/v{version}/predict', methods=['POST'])
+# @app.route('/carbon/1/predict', methods=['POST'])
 def predict():
     try:
         features_trans = trans.transform(np.array(request.json['prediction']).reshape(1,55))
